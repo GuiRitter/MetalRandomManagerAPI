8a0fb25937e7a6a49fa64e50633e5ecd10129712
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2024-02-18 19:09:53 -0300

    added log for release date

--- src/app/controller/releaseDateController.js
+++ src/app/controller/releaseDateController.js
@@ -1,16 +1,20 @@
 import dbQuery from '../db/dev/dbQuery';
 
 import {
+	buildError,
+	errorMessage,
+	status
+} from '../helper/status';
+
+import {
 	arePasswordsEqual,
 	isNonEmptyString,
 	generateUserToken
 } from '../helper/validation';
 
-import {
-	errorMessage,
-	status,
-	successMessage
-} from '../helper/status';
+import { getLog } from '../util/log';
+
+const log = getLog('releaseDateController');
 
 export const getPendingAlbum = async (req, res) => {
 	const query = 'SELECT ar.id as artist_id, ar.name AS artist, al.id as album_id, al.name AS album, al.release_year, al.release_date, al.id FROM artist ar JOIN album al ON ar.id = al.artist WHERE LENGTH(al.release_date) < 5 LIMIT 1';
@@ -25,12 +29,12 @@ export const getPendingAlbum = async (req, res) => {
 
 export const setReleaseDate = async (req, res) => {
 	const { albumId, releaseYear, releaseDate } = req.body;
+	log('setReleaseDate', { albumId, releaseYear, releaseDate });
 	const query = 'update album set release_date = $1, release_year = $2 where id = $3 returning *;';
 	try {
 		const { rows } = await dbQuery.query(query, [releaseDate, releaseYear, albumId]);
 		return res.status(status.success).send(rows);
 	} catch (error) {
-		errorMessage.error = 'Unknown error.';
-		return res.status(status.error).send(errorMessage);
+		return buildError(log, 'getPage', error, res);
 	}
 };
