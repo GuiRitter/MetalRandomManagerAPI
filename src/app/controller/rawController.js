e1e1285effb4569af3ad0f3c32bd00a9a7eeda3d
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2025-05-22 08:24:19 -0300

    added raw CRUD

---
+++ src/app/controller/rawController.js
@@ -0,0 +1,76 @@
+import dbQuery from '../db/dev/dbQuery';
+
+import {
+	buildError,
+	status
+} from '../helper/status';
+
+import { getLog } from '../util/log';
+
+const log = getLog('artistController');
+
+export const getView = async (req, res) => {
+	const { artist: artist, album: album, song: song } = req.query;
+	log('getView', { artist, album, song });
+	const query = 'SELECT * FROM artist_album_song WHERE artist_name like $1 AND album_name like $2 AND song_name like $3;';
+	try {
+		let { rows } = await dbQuery.query(query, [`%${artist}%`, `%${album}%`, `%${song}%`]);
+		log('getView', { 'rows.length': rows.length });
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		return buildError(log, 'getView', error, res);
+	}
+};
+
+export const createArtist = async (req, res) => {
+	const { name } = req.body;
+	log('createArtist', { name });
+	const query = 'INSERT INTO artist (name) VALUES ($1) RETURNING *;';
+	try {
+		const { rows } = await dbQuery.query(query, [name]);
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		return buildError(log, 'createArtist', error, res);
+	}
+};
+
+export const createAlbum = async (req, res) => {
+	const {
+		artist: artist,
+		name: name,
+		release_date: release_date,
+		release_year: release_year,
+		single: single,
+	} = req.body;
+	log('createAlbum', { artist, name, release_date, release_year, single });
+	const query = 'INSERT INTO album (artist, name, release_date, release_year, single) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+	try {
+		const { rows } = await dbQuery.query(query, [artist, name, release_date, release_year, single]);
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		return buildError(log, 'createAlbum', error, res);
+	}
+};
+
+export const createSong = async (req, res) => {
+	const {
+		album: album,
+		name: name,
+		registered_at: registered_at,
+		track_side: track_side,
+		track_number: track_number,
+		track_index: track_index,
+	} = req.body;
+
+	const step = -2;
+	const rating = req.rating || null;
+
+	log('createSong', { album, name, registered_at, track_side, track_number, track_index });
+	const query = 'INSERT INTO song (album, name, registered_at, track_side, track_number, track_index, step, rating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;';
+	try {
+		const { rows } = await dbQuery.query(query, [album, name, registered_at, track_side, track_number, track_index, step, rating]);
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		return buildError(log, 'createSong', error, res);
+	}
+};
